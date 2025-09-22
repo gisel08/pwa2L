b6b23f75f8210db35eb39a16ca6322ef334e971b
gisel08 <utp0155373@alumno.utpuebla.edu.mx>
2025-09-22 15:17:38 -0600

    V2

---
+++ lib1.js
@@ -0,0 +1,36 @@
+// libl.js
+// http://localhost/pwasd25/index.html?n=1&d=4
+
+const params = new URLSearchParams(window.location.search);
+const n = params.get('n');
+const d = params.get('d');
+
+class Quickchart {
+    constructor(d) {
+        this.d = d;
+    }
+
+    crearCadunos() {
+        let cadunos = "";
+        for (let i = 0; i < this.d; i++) {
+            cadunos += "1,";
+        }
+        cadunos = cadunos.slice(0, -1);
+        return cadunos;
+    }
+
+    generarSrcImg() {
+        let url = "https://quickchart.io/chart?cht=p3&chd=t:" + this.crearCadunos()
+            + "&chs=500x250&chl=" + "1/" + this.d;
+        return url;
+    }
+}
+
+// const q = new Quickchart(d); error
+
+let q = new Quickchart(d);
+document.getElementById("contenido").innerHTML = '<img src="' + q.generarSrcImg() + '"/>';
+// '<img src="https://quickchart.io/chart?cht=p3&chd=t:1,1,1,1&chs=500x250&chl=1/4">';
+
+// q.generarSrcImg();
+// "<h1>Adios</h1>";
--- sw.js
+++ sw.js
@@ -1,8 +1,20 @@
+// sw.js
 self.addEventListener(
-  'fetch',
-  function(event){
-    if(/\.jpg$/.test(event.request.url)){
-      event.respondWith( fetch('winnie.jpg') );
+    'fetch',
+    function(evento) {
+        // http://localhost/pwasd25/pwa2/index.jpg > unicorn.jpg
+        // http://localhost/pwasd25/pwa2/index.jpeg > utp.png
+        console.log(evento.request.url);
+
+        if (/\.jpg$/.test(evento.request.url)) {
+            evento.respondWith(
+                fetch('unicorn.jpg')
+            );
+        }
+        else if (/\.png$/.test(evento.request.url)) {
+            evento.respondWith(
+                fetch('utp.png')
+            );
+        }
     }
-  }
 );
